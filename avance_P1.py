89aa1b8e1ae755928c8f82d5b662fe30684d46b7
lolli09 <70044248+lolli09@users.noreply.github.com>
2020-08-28 23:53:35 -0500

    Add files via upload
    Avance 1 del proyecto (funciones).

---
+++ avance_P1.py
@@ -0,0 +1,23 @@
+materias=int(input("¿Cuántas materias estás cursando?"))
+def num_materias(materias):
+    return (materias)
+
+beca=str(input("¿Tienes beca?"))
+def beca_sn(beca):
+    return (beca)
+
+tareas=int(input("¿Cuál es el porcentaje de tus tareas?"))
+def por_tareas(tareas):
+    return (tareas)
+
+examen=int(input("¿Cuál es el porcentaje de tu examen?"))
+def por_examen(examen):
+    return (examen)
+
+actividades=int(input("¿Cuál es el porcentaje de tus actividades?"))
+def por_actividades(actividades):
+    return (actividades)
+
+proyecto=int(input("¿Cuál es el porcentaje de tu proyecto?"))
+def por_proyecto(proyecto):
+    return (proyecto)
